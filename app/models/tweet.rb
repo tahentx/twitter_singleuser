80a6032cada7d9048c7b8b6a18b353c66443c1e7
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-20 10:32:48 -0500

    add create uuid action to tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,10 @@
 class Tweet < ApplicationRecord
+	before_create :create_uuid
 	validates :status, length: { maximum: 140, minimum: 1 }
+	private
+	def create_uuid
+	    begin
+	      self.uuid = SecureRandom.uuid
+	    end while self.class.exists?(:uuid => uuid)
+    end
 end
