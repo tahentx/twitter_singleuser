b43f6686ea15dfa7c8f2bb814dd1614fd2bed63a
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-20 14:32:20 -0500

    update sessions controller to include current user method

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620153632) do
+ActiveRecord::Schema.define(version: 20170620182705) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uuid"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
