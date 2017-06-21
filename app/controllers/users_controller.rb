3f8b908403f342544ce143e5a378d87a9c27c6c5
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-21 09:32:07 -0500

    add create method on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,14 @@ class UsersController < ApplicationController
   	  alert: "Your log in was denied"
   	  redirect_to login_path
   	else
-  	  if !(User.find_by(auth_hash.info.twitter))
-  	  	@user = User.create_user
+  	  if !(User.find_by(auth_hash.info.nickname))
+  	  	@user = User.create_user(auth_hash)
+  	  else
+  	  	@user = User.find_by(auth_hash.info.nickname)
+  	  end
+  	  session[:user_id] = @user.id
+  	  redirect_to tweets_path
+  end
 
   protected
 
