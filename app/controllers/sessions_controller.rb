4fbf1ef9973b8d8bf5b4cae8c8f505e451565ecb
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-21 09:38:30 -0500

    add create and failure actions to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
  def create
+ 	if current_user
+ 		redirect_to new_tweet_path
+ 	end
  end
+
+  def failure
+	redirect_to root_path, alert: "Your log in was denied"
+  end
 
   def destroy
   	session[:user_id] = nil
   	redirect_to root_path, alert: "You have logged out."
-  end
-
-  protected
-
-  def auth_hash
-    request.env['omniauth.auth']
   end
 end
